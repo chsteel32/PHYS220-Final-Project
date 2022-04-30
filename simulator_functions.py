c2adde581388c60c7e63b4329e5dd0cf5d92f3eb
chsteel32 <chsteel32@gmail.com>
2022-04-30 18:14:40 -0400

    Fixed error where only the last initial condition was applied. Now all ics are properly included in the netlist.

--- simulator_functions.py
+++ simulator_functions.py
@@ -9,15 +9,17 @@ import os
 
 def transient_simulator(circuit, start_time, step_time, end_time, analyzed_nodes, initial_conditions):
 
-    print(circuit)
-
-    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
-
     for current_node in initial_conditions:
 
         ic = initial_conditions[current_node]
 
-        circuit.raw_spice = '.ic v({})={}'.format(current_node, ic)
+        circuit.raw_spice += '.ic v({})={}@u_V'.format(current_node, ic)
+
+        print('ic v({}) = {}'.format(current_node, ic))
+
+    simulator = circuit.simulator(temperature=25, nominal_temperature=25)
+
+    print(simulator)
 
     analysis = simulator.transient(start_time=float(start_time)@u_s, step_time=float(step_time)@u_us, end_time=float(end_time)@u_s)
 
