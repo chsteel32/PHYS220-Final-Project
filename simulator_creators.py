dc0f0257bf5a1882fd69c539b41f5d6f1775af77
chsteel32 <chsteel32@gmail.com>
2022-04-30 00:28:44 -0400

    Added functionality for initial conditions. There is still an error to be fixed where the initial condition values are not properly update checked.

--- simulator_creators.py
+++ simulator_creators.py
@@ -2,11 +2,36 @@ from tkinter import *
 import add_element_functions as aef
 import simulator_functions as sf
 
+circuit_ground_options = ['circuit.gnd', 'ground', 'g', 'gr', '0']
+
+
+def create_string_var(current_window, trace_function_1, trace_function_2):
+
+    string_var = StringVar(current_window)
+    string_var.trace('w', trace_function_1)
+    string_var.trace('w', trace_function_2)
+
+    return string_var
+
+
+def get_initial_conditions(variable_dictionary):
+
+    init_cons = {}
+
+    for node in variable_dictionary:
+
+        init_cons[node] = variable_dictionary[node].get()
+
+    return init_cons
+
 
 def create_transient_simulator(circuit):
 
     node_check_boxes = {}
     var_vals = {}
+
+    initial_conditions = {}
+    initial_conditions_variables = {}
 
     def send_nodes(check_nodes, current_var_values):
 
@@ -47,6 +72,8 @@ def create_transient_simulator(circuit):
             float(start_time_value.get())
             float(step_time_value.get())
             float(end_time_value.get())
+            for current_node in initial_conditions:
+                float(initial_conditions[current_node].get())
             error_label.config(text='')
         except ValueError:
             error_label.config(text='Voltage Value must be a number!')
@@ -89,24 +116,53 @@ def create_transient_simulator(circuit):
 
         var_vals[node] = current_var
 
-        current_check = Checkbutton(transient_creator_window, text='{}'.format(node), variable=current_var,
+        if str(node).lower() in circuit_ground_options:
+
+            pass
+
+        else:
+
+            current_check = Checkbutton(transient_creator_window, text='{}'.format(node), variable=current_var,
                                     justify=LEFT)
 
-        current_check.grid(column=1, row=4+counter)
-        current_check.grid_propagate(False)
+            current_check.grid(column=1, row=4+counter)
+            current_check.grid_propagate(False)
 
-        node_check_boxes[node] = current_check
+            node_check_boxes[node] = current_check
 
-        counter += 1
+            counter += 1
 
-    print(node_check_boxes)
+    Label(transient_creator_window, text='Initial Conditions: ').grid(column=0, row=4+counter)
+
+    for node in aef.circuit_nodes:
+
+        string_var = create_string_var(transient_creator_window, check_fields, validate_input)
+
+        initial_conditions_variables[node] = string_var
+
+        if str(node).lower() in circuit_ground_options:
+
+            pass
+
+        else:
+
+            Label(transient_creator_window, text=node).grid(column=0, row=5+counter)
+            initial_condition = Entry(transient_creator_window, width=5, textvariable=string_var)
+
+            initial_condition.grid(column=1, row=5+counter)
+            initial_condition.grid_propagate(False)
+
+            initial_conditions[node] = initial_condition
+
+            counter += 1
 
     simulation_button = Button(transient_creator_window, text='Start Simulation',
                                command=lambda: [sf.transient_simulator(circuit, start_time_value.get(),
                                                                        step_time_value.get(), end_time_value.get(),
-                                                                       send_nodes(node_check_boxes, var_vals)),
+                                                                       send_nodes(node_check_boxes, var_vals),
+                                                                       get_initial_conditions(initial_conditions)),
                                                 transient_creator_window.destroy()], state=DISABLED)
-    simulation_button.grid(column=1, row=4+counter)
+    simulation_button.grid(column=1, row=5+counter)
 
     error_label = Label(transient_creator_window, text='')
-    error_label.grid(column=1, row=5+counter, padx=2, pady=2)
+    error_label.grid(column=1, row=6+counter, padx=2, pady=2)
